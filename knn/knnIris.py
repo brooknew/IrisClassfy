d044f2cfa10542561dcf6c95262fa92d31460323
brooknew <brooknew@163.com>
2018-07-11 13:57:55 +0800

    tiny change

--- knn/knnIris.py
+++ knn/knnIris.py
@@ -1,10 +1,14 @@
 
 def getDataFromDatafile(n1, n2):
+    '''out n2-n1 data items 
+    each item of out data include:
+    sepal length,sepal width,petal length,petal width,target . Example:
+    5.4,         3.9,        1.3,         0.4,        0
+    '''
     with open( '../data/irisRandom.csv' , 'rt') as fr :   
         for i  in range(66) :
             buf = fr.readline()
-        dataGroup = []
-        
+        dataGroup = []        
         for i in range( n2 ) :
             line = fr.readline()
             if( i >= n1 ) : 
@@ -14,34 +18,31 @@ def getDataFromDatafile(n1, n2):
                 for item in line[:-1]:
                     items.append(  float( item ) )
                 items.append( int( line[-1] ) )
-                dataGroup.append( items )
-        
+                dataGroup.append( items )        
         return dataGroup
 
-class knn():
+class knn():#'''KNN classificator'''
     def __init__(self) :
         self.data = []
     def fit( self , data ) :
         self.data = data
     def testData(self) :
         print( self.data )
-    def pred( self ,k , item ) :
+    def pred( self ,k , item ) :#'''k is the neighbor(s) , item is the feature tuple to be predicted'''
         distIrisClass = []
-        i= 1
         for t in self.data :
             dis = 0 
             for tt1 , tt2 in zip ( t[0:-1] , item ): 
                 d = tt1 - tt2 
                 d *= d
                 dis += d
-            i = i + 1
             dc = [ dis , t  ]
             distIrisClass.append( dc )
         distIrisClass.sort()
         subl = distIrisClass[0:k]
         kind = {0:0,1:0,2:0}
         for it in subl:
-            kind[ int(  it[1][4]) ] += 1
+            kind[ int( it[1][4]) ] += 1
         max = 0
         maxK = -1
         for i , v  in kind.items() :
@@ -82,7 +83,7 @@ def predSome( data , knns , k  ) :
     good1 = good/len( res )*100
     print ( good, ' right ',  good1 , '%' )
     saveTest( res ) 
-    return good
+    return good1
     
 
 def main() :
@@ -91,8 +92,8 @@ def main() :
     knns.fit( data ) 
     knns.pred( 5 , [5.7,3.1,2.6,1.2] )
     data = getDataFromDatafile(112,150)
-    predSome( data , knns , 5  ) 
-    
+    r = predSome( data , knns , 5  ) 
+    print( 'knn successfull rate:' , r )
 
 
 main()
