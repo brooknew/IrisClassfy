65ddf51bbf2dd13c82ade26d733178b198956e1f
brooknew <brooknew@163.com>
2018-07-11 13:41:07 +0800

    knn

--- data/irisdata2csv.py
+++ data/irisdata2csv.py
@@ -1,8 +1,11 @@
 from sklearn.datasets import load_iris
+import random
+
 
 iris_dataset = load_iris()
 
 with open( 'iris.csv' , 'wt') as fw :
+    fw1 = open( 'irisRandom.csv' , 'wt') 
     descr = iris_dataset['DESCR']
     t = descr.split('\n' )
     lines =  len( t )
@@ -34,12 +37,25 @@ with open( 'iris.csv' , 'wt') as fw :
     targD = iris_dataset['target']
     targetNames = iris_dataset['target_names']
     #print( targetNames )
+    wline = [] 
     for f , t in zip( feaD, targD ) :
         linS= str(f[0])+','+ str(f[1])+','+ str(f[2])+',' + str(f[3])+','
         linS +=  str(t) + ',' +  targetNames[t]
         #print( linS )
-        fw.write( linS+'\n' ) 
-
+        fw.write( linS+'\n' )
+        wline.append( linS )
+        
+    fw1.write( content )
+    index = [ x  for x in range( 150 ) ]
+    random.shuffle( index )
+    random.shuffle( index )
+    random.shuffle( index )
+    random.shuffle( index )
+    random.shuffle( index )
+    random.shuffle( index )
+    for i in range( 150 ) :
+        fw1.write( wline[ index[i] ] +'\n') 
+    fw1.close() 
 
 
     
--- knn/knnIris.py
+++ knn/knnIris.py
@@ -1,21 +1,21 @@
 
-
-def getDataFromDatafile():
-    with open( '../data/iris.csv' , 'rt') as fr :   
+def getDataFromDatafile(n1, n2):
+    with open( '../data/irisRandom.csv' , 'rt') as fr :   
         for i  in range(66) :
             buf = fr.readline()
-            #print(i+1, ':' , buf ,end='' )
-
         dataGroup = []
-        for i in range(150) :
+        
+        for i in range( n2 ) :
             line = fr.readline()
-            line = line[0:-1].split( ',' )
-            line = line[0:-1]
-            items = [] 
-            for item in line:
-                items.append(  float( item ) )
-            #print( items )
-            dataGroup.append( items )
+            if( i >= n1 ) : 
+                line = line[0:-1].split( ',' )
+                line = line[0:-1]
+                items = [] 
+                for item in line[:-1]:
+                    items.append(  float( item ) )
+                items.append( int( line[-1] ) )
+                dataGroup.append( items )
+        
         return dataGroup
 
 class knn():
@@ -31,37 +31,68 @@ class knn():
         for t in self.data :
             dis = 0 
             for tt1 , tt2 in zip ( t[0:-1] , item ): 
-                d = tt1 - tt2
-                #print ( 'd ' , d ) 
+                d = tt1 - tt2 
                 d *= d
                 dis += d
-            #print( i ,':', dis , t )
             i = i + 1
             dc = [ dis , t  ]
             distIrisClass.append( dc )
         distIrisClass.sort()
-        for i in distIrisClass:
-            print ( i )
         subl = distIrisClass[0:k]
         kind = {0:0,1:0,2:0}
-        print( 'subl:' , subl )
         for it in subl:
-            print( int( it[1][4]) )
             kind[ int(  it[1][4]) ] += 1
-        #kind1 = [ v for v in sorted(kind.values())]
-        print ( kind ) 
+        max = 0
+        maxK = -1
+        for i , v  in kind.items() :
+            if v  > max :
+                max = v
+                maxK = i
+        return maxK 
          
-           
-    
+def saveTest( record ) :
+    ''' record format:
+        sepal length,sepal width,petal length,petal width,target,pred target,pred right?
+    '''
+    with open( '../data/test.csv' , 'wt') as fw:
+        fw.write('sepal length,sepal width,petal length,petal width,target,pred target,pred right?\n')
+        for it in record:
+            s = ''
+            for i in it[:-1]:
+                s += str(i)
+                s += ','
+            s += str(it[-1])
+            s += '\n'
+            fw.write( s ) 
+            
+
+
+def predSome( data , knns , k  ) :
+    res = []
+    for da in data :
+        pred = knns.pred(  k , da )
+        da.append( pred )
+        flag = pred == da[4]
+        da.append( flag )
+        res.append( da )
+    good = 0. 
+    for da in res :
+        if da[6] :
+            good += 1
+    good1 = good/len( res )*100
+    print ( good, ' right ',  good1 , '%' )
+    saveTest( res ) 
+    return good
     
 
 def main() :
-    data = getDataFromDatafile()
-    print( 'len of data:' , len( data  ))
+    data = getDataFromDatafile(0,112)
     knns = knn( )
     knns.fit( data ) 
-    knns.testData()
     knns.pred( 5 , [5.7,3.1,2.6,1.2] )
+    data = getDataFromDatafile(112,150)
+    predSome( data , knns , 5  ) 
+    
 
 
 main()
